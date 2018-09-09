d57d2f62356e3e48a2cda2ff6131489d0b5b3753
Kritika Rathore <kritikar90@gmail.com>
2018-09-09 12:48:23 +1200

    Sample Kendo Project

---
+++ Attendance_System/Attendance_System/Controllers/GridController.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web.Mvc;
+using Kendo.Mvc.Extensions;
+using Kendo.Mvc.UI;
+using Attendance_System.Models;
+using System.Data;
+
+namespace Attendance_System.Controllers
+{
+	public partial class GridController : Controller
+    {
+        private Attendance_Sys_Entities db = new Attendance_Sys_Entities();
+        private int Cls_Sch_ID = 0;
+        public ActionResult ClassSubject_Read([DataSourceRequest]DataSourceRequest request)
+		{
+            var result = db.GetSubjectByClassId().ToList();
+
+			return Json(result.ToDataSourceResult(request));
+		}
+        public ActionResult Student_Read([DataSourceRequest]DataSourceRequest request,string ClassScheduleID)
+        {
+            Cls_Sch_ID = Convert.ToInt32(ClassScheduleID);
+            var result = db.GetStudentByClassScheduleId(Cls_Sch_ID).ToList();
+
+            return Json(result.ToDataSourceResult(request));
+        }
+        public ActionResult Student_Create([DataSourceRequest]DataSourceRequest request, Student st)
+        {
+            
+            db.InsertStudent(st.FirstName, st.MiddleName, st.LastName, st.Gender, st.DateofBirth, st.AddressLine1, st.AddressLine2, st.City, st.Country, Convert.ToInt32(st.PhoneNo), 1,"P");
+            db.SaveChanges();
+            var result = db.GetStudentByClassScheduleId(Cls_Sch_ID).ToList();
+            return Json(result.ToDataSourceResult(request));
+        }
+        public ActionResult Student_Update([DataSourceRequest]DataSourceRequest request, Student st)
+        {
+            db.UpdateStudentDetails(Convert.ToInt32(st.StudentID),st.FirstName,st.MiddleName,st.LastName,st.Gender,st.DateofBirth,st.AddressLine1,st.AddressLine2,st.City,st.Country,Convert.ToInt32(st.PhoneNo));
+            db.SaveChanges();
+            var result = db.GetStudentByClassScheduleId(Cls_Sch_ID).ToList();
+            return Json(result.ToDataSourceResult(request));
+        }
+        public ActionResult Student_Destroy([DataSourceRequest]DataSourceRequest request, string StudentID,string AttendanceID)
+        {
+            var stID = Convert.ToInt32(StudentID);
+            Student st = db.Students.Find(stID);
+            if (st == null)
+            {
+                return Json("Student Not Found");
+            }
+
+            db.Students.Remove(st);
+            db.SaveChanges();
+            //var stID = Convert.ToInt32(StudentID);
+            //db.DeleteStudent(stID);
+            //db.SaveChanges();
+            var result = db.GetStudentByClassScheduleId(Cls_Sch_ID).ToList();
+            return Json(result.ToDataSourceResult(request));
+        }
+    }
+}
---
+++ Attendance_System/Attendance_System/Scripts/AttendanceResponse.js
@@ -0,0 +1,45 @@
+﻿function change() {
+    //var grid = $("#ClassSubjectGrid").data("kendoGrid");
+    //var cls_sch_ID = grid.dataItem(grid.select()).ClassScheduleID;
+    ////alert(cls_sch_ID);
+    
+    $("#StudentGrid").data("kendoGrid").dataSource.read();
+}
+
+function data() {
+    var grid = $("#ClassSubjectGrid").data("kendoGrid");
+    var cls_sch_ID = grid.dataItem(grid.select()).ClassScheduleID;
+    //alert(cls_sch_ID);
+    return {
+        ClassScheduleID: cls_sch_ID
+    };
+}
+function UpdateInsertdata() {
+    var grid = $("#StudentGrid").data("kendoGrid");
+    var cls_sch_ID = { ClassScheduleID: grid.dataItem(grid.select()).ClassScheduleID };
+    var st = {
+        StudentID: grid.dataItem(grid.select()).StudentID,
+        FirstName: grid.dataItem(grid.select()).FirstName,
+        MiddleName: grid.dataItem(grid.select()).MiddleName,
+        LastName: grid.dataItem(grid.select()).LastName,
+        Gender: grid.dataItem(grid.select()).Gender,
+        DateofBirth: grid.dataItem(grid.select()).DateofBirth,
+        AddressLine1: grid.dataItem(grid.select()).AddressLine1,
+        AddressLine2: grid.dataItem(grid.select()).AddressLine2,
+        City: grid.dataItem(grid.select()).City,
+        Country: grid.dataItem(grid.select()).Country,
+        PhoneNo: grid.dataItem(grid.select()).PhoneNo,
+    }
+    //alert(cls_sch_ID);
+    return st;
+}
+function deletedata() {
+    var grid = $("#StudentGrid").data("kendoGrid");
+    var st_ID = grid.dataItem(grid.select()).StudentID;
+    var att_id = grid.dataItem(grid.select()).AttendanceID;
+    //alert(cls_sch_ID);
+    return {
+        StudentID: stID,
+        AttendanceID : att_id
+    };
+}
